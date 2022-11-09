094d9c2aa6d5c1430b05c3a605327a47fb7905f6
tania-silva <61155949+tania-silva@users.noreply.github.com>
2022-11-09 17:50:47 +0000

    INSERT & UPDATE keywords

--- www/MP_LECT_SNA_TchMatAdd2.php
+++ www/MP_LECT_SNA_TchMatAdd2.php
@@ -32,6 +32,7 @@ $vidId=$_GET["idVideo"];
 
 $fromPage="";
 $fromPage=$_GET["from"];
+$keywords=[];
 
 $validateValue=0;
 if (isset($_POST['examine'])) $validateValue=4;
@@ -55,7 +56,10 @@ if ($_GET["act"]=="reg" AND $usrId) {
 
 	while ($row=mysqli_fetch_array($result)) { 
 		$id_key=$row["id"];
-		if ($_POST["key".$id_key]==$id_key) $videoKeywords.=$id_key."_";
+		if ($_POST["key".$id_key]==$id_key){
+			$videoKeywords.=$id_key."_";
+			array_push($keywords, $id_key);
+		} 
 	}
 
 	$checkStatus=0;
@@ -75,6 +79,19 @@ if ($_GET["act"]=="reg" AND $usrId) {
 				keywords='$videoKeywords'
 			WHERE id=$vidId";
 		$result=mysqli_query($conn,$sql);
+
+		//IPB: Add keywords to platform_keyword_videoreviews
+		$sqlDelete = " 
+		DELETE FROM `platform_keyword_snatchmaterials` WHERE `id_sna_tchmaterials` = '$vidId'";
+		$resultDelete=mysqli_query($conn,$sqlDelete);
+
+		$sqlInsert= "
+		INSERT INTO `platform_keyword_snatchmaterials`(`id_keyword`, `id_sna_tchmaterials`) VALUES";
+		foreach($keywords as $key){
+			$sqlInsert .= "('$key','$vidId'),";
+		}
+		$sqlInsert= rtrim($sqlInsert, ",");
+		$resulInsert=mysqli_query($conn,$sqlInsert);
 		
 		if ($fromPage=="edit") $redirectUrl="./MP_LECT_SNA_TchMatEdit.php?id_act=".$vidId; // passa alla pagina di edit
 		elseif ($fromPage=="valedit" OR $fromPage=="toggle") $redirectUrl="./MP_LECT_SNA_TchMatValidateEdit.php?from=".$fromPage."&id_act=".$vidId; // passa alla pagina di edit
@@ -121,7 +138,7 @@ if ($_GET["act"]=="reg" AND $usrId) {
 					<?php }?>
 
 					<form method="post" action="./MP_LECT_SNA_TchMatAdd2.php?act=reg&idVideo=<?=$vidId?>&from=<?=$fromPage?>" enctype="multipart/form-data" style="display: block;margin-top: 5px;padding: 20px 0 20px 50px;border: solid 1px #00aeef;border-radius: 10px;">
-
+						<?php var_dump($keywords);?>
 						<p>Please select keywords for this video reviews</p>
 
 						<div style="padding-top: 25px;">
